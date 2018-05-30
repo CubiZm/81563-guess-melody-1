ce570cf1770261d735e0840615c65abb0673918c
CubiZm <karelense@gmail.com>
2018-05-30 12:33:19 +0300

    add next and previous btn, add function for btn

--- js/main.js
+++ js/main.js
@@ -5,6 +5,26 @@ const KeyCode = {
   RIGHT: 39,
 };
 
+const arrowsElementNode = document.createElement(`div`);
+arrowsElementNode.className = `arrows__wrap`;
+arrowsElementNode.innerHTML = `
+  <style>
+    .arrows__wrap {
+      position: absolute;
+      top: 135px;
+      left: 50%;
+      margin-left: -56px;
+    }
+    .arrows__btn {
+      background: none;
+      border: 2px solid black;
+      padding: 5px 20px;
+    }
+  </style>
+  <button class="arrows__btn" id="previous"><-</button>
+  <button class="arrows__btn" id="next">-></button>
+`;
+
 const startScreen = `main--welcome`;
 const template = document.querySelector(`#templates`).content;
 const screenElements = [...template.querySelectorAll(`.main`)];
@@ -12,7 +32,6 @@ const appElement = document.querySelector(`.app`);
 const startScreenIndex = screenElements.findIndex((screenElement) => screenElement.classList.contains(startScreen));
 const maxArrayLength = screenElements.length;
 let currentIndex = startScreenIndex;
-
 
 const showScreen = (index) => {
 
@@ -23,6 +42,7 @@ const showScreen = (index) => {
   const stepContent = screenElements[index];
   appElement.innerHTML = ``;
   appElement.appendChild(stepContent);
+  appElement.insertAdjacentElement(`beforeend`, arrowsElementNode);
 };
 
 const turn = (isLeft = true) => {
@@ -43,9 +63,26 @@ const onKeyDown = (evt) => {
   if (evt.keyCode === KeyCode.LEFT) {
     turn(true);
   }
+
+  return;
+};
+
+const onClick = (evt) => {
+  const nextArrow = document.getElementById(`next`);
+  const previousArrow = document.getElementById(`previous`);
+
+  if (evt.target === nextArrow) {
+    turn(false);
+  }
+
+  if (evt.target === previousArrow) {
+    turn(true);
+  }
+
   return;
 };
 
 document.addEventListener(`keydown`, onKeyDown);
+document.addEventListener(`click`, onClick);
 
 showScreen(currentIndex);
